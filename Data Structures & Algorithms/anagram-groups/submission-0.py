9037b246ffb541654c0de27845b8bc88523e1005
Quan Nguyen <59292938+QuanDev2@users.noreply.github.com>
2026-05-24 16:32:40 -0400

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.py
@@ -0,0 +1,30 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # go through strs (n length)
+        # each str, sorted
+        s_strs = []
+        for str in strs:
+            s_strs.append("".join(sorted(str))) # O(mlog(m)) time and O(m) space; m being length of the longest str
+            
+
+        # temp result: hashmap of lists; convert later
+        result = {}
+        for i, s_str in enumerate(s_strs): # ['act', 'opst', 'opst', 'act', 'opst', 'aht']
+            if s_str in result:
+                result[s_str].append(i)
+            else:
+                result[s_str] = [i]
+        
+        result_list = []
+        for s_str, idx_list in result.items():
+            anagrams = []
+            for i in idx_list: # [0, 2, 3]
+                anagrams.append(strs[i])
+            result_list.append(anagrams)
+
+        return result_list
+        
+        
+        
+        
+        
