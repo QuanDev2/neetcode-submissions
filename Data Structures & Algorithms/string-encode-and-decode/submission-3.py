368ceb56feda7208352ee289de1a7d265c6a96e9
Quan Nguyen <59292938+QuanDev2@users.noreply.github.com>
2026-05-24 22:18:00 -0400

    Add: string-encode-and-decode - submission-3

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.py
@@ -0,0 +1,27 @@
+# Time and space O(n*m)
+class Solution:
+    def encode(self, strs: List[str]) -> str:
+        # use "length#string"
+        res = ""
+        for s in strs:
+            res += str(len(s))
+            res += "#"
+            res += s
+
+        return res
+        
+    def decode(self, s: str) -> List[str]:
+        # s = "3#cab2#hi"
+        res = []
+        i = 0
+        while i < len(s):
+            j = i + 1
+            while s[j] != '#':
+                j += 1
+            s_len = int(s[i:j])
+            i = j + 1 # i = 2
+            j = i + s_len # j = 5
+            res.append(s[i:j])
+            i = j
+        
+        return res
