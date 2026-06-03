aacbb8490f87fdfa7b55a8be8bd4b4908ba39dd8
Quan Nguyen <59292938+QuanDev2@users.noreply.github.com>
2026-06-03 18:01:43 -0400

    Add: time-based-key-value-store - submission-1

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-1.py
@@ -0,0 +1,31 @@
+class TimeMap:
+    """
+    hashmap key -> [(timestamp, value)]
+    retrieve val, then binary search
+    """
+
+    def __init__(self):
+        self.hashmap = defaultdict(list)
+               
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.hashmap[key].append((timestamp, value))      
+
+    def get(self, key: str, timestamp: int) -> str:
+        if key not in self.hashmap:
+            return ""
+        vals = self.hashmap[key]
+        i, j = 0, len(vals)-1
+        while i <= j:
+            mid = (i+j)//2
+            if vals[mid][0] == timestamp:
+                return vals[mid][1]
+            elif timestamp < vals[mid][0]:
+                j = mid - 1
+            else:
+                i = mid + 1
+
+        return vals[j][1] if j >= 0 else ""
+
+"""
+(1, 'nice'), (2, 'yo'),TARGET, (4, 'here'), (7, 'last'))
+"""
