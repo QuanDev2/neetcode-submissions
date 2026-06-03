f737b5baa66867068a6d5499fc8311232c4b183a
Quan Nguyen <59292938+QuanDev2@users.noreply.github.com>
2026-06-03 10:18:30 -0400

    Add: find-minimum-in-rotated-sorted-array - submission-0

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-0.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        # find the pivot point, which is the point where left > mid > right
+        # [2,3,4,5,1] >
+        # [3,4,5,1,2] >
+        # [1,2,3,4,5] 
+        # [4,5,1,2,3] 
+        # [5,1,2,3,4] 
+        # observation: if min < right => right half is sorted => pivot is on left half. pivot is min
+        i, j = 0, len(nums)-1
+        while i < j:
+            m = (i+j) // 2
+            if nums[m] > nums[j]:
+                # min is on right half -> move right
+                i = m+1
+            else:
+                j = m
+            
+        return nums[i]
+        
