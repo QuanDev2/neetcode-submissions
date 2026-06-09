22eafe6ef70507797f02761163aadb6d80fecdc2
Quan Nguyen <59292938+QuanDev2@users.noreply.github.com>
2026-06-08 23:12:49 -0400

    Add: lru-cache - submission-1

---
+++ Data Structures & Algorithms/lru-cache/submission-1.py
@@ -0,0 +1,64 @@
+"""
+Use a doubly linked list to store the nodes
+head end = lru
+tail end = mru
+use in tandem with hashmap for fast lookup
+get(): move node to tail
+put(): update or create, then move node to tail
+exceed cap: cut off head
+"""
+
+class LRUCache:
+    class Node:
+        def __init__(self, key: int, val: int = 0):
+            self.key = key
+            self.val = val
+            self.prev = None
+            self.next = None
+
+    def __init__(self, capacity: int):
+        self.cap = capacity
+        self.hashmap = {}
+        self.head, self.tail = self.Node(0), self.Node(0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def get(self, key: int) -> int:
+        if key in self.hashmap:
+            # move node to tail
+            mru = self.hashmap[key]
+            self.remove(mru)
+            self.insertAtTail(mru)
+            return self.hashmap[key].val
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        # if key exists, update val in hashmap
+        if key in self.hashmap:
+            node = self.hashmap[key]
+            node.val = value
+            self.remove(node)
+            self.insertAtTail(node)
+        else:
+        # key not exists, create node, add to hashmap
+            node = self.Node(key, value)
+            self.hashmap[key] = node
+            self.insertAtTail(node)
+
+        # check size, if exceed cap, remove from hashmap, cut off head
+        if len(self.hashmap) > self.cap:
+            lru = self.head.next
+            self.hashmap.pop(lru.key)
+            self.head.next = lru.next
+            lru.next.prev = self.head
+
+    def remove(self, node):
+        node.prev.next = node.next
+        node.next.prev = node.prev
+
+    def insertAtTail(self, node):
+        self.tail.prev.next = node
+        node.prev = self.tail.prev
+        node.next = self.tail
+        self.tail.prev = node
+
