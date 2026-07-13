5a7463efab2ad84c992c5ba07b265bc83a3666f6
Quan Nguyen <59292938+QuanDev2@users.noreply.github.com>
2026-07-12 21:48:48 -0400

    Add: house-robber-ii - submission-4

---
+++ Data Structures & Algorithms/house-robber-ii/submission-4.py
@@ -0,0 +1,11 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        def robMax(split_nums: List[int]) -> int:
+            two, one = 0, 0
+            for n in split_nums:
+                two, one = one, max(two + n, one)
+            return one
+
+        if len(nums) == 1:
+            return nums[0]
+        return max(robMax(nums[1:]), robMax(nums[:-1]))
