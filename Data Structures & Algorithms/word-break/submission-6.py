b221083e1a743a0a58c6d0f85559c741b2160935
Quan Nguyen <59292938+QuanDev2@users.noreply.github.com>
2026-07-15 14:58:27 -0400

    Add: word-break - submission-6

---
+++ Data Structures & Algorithms/word-break/submission-6.py
@@ -0,0 +1,16 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        # for every s[i], check all the words w in wordDict to see if we can fit w from i-th pos onward
+        # dp[i] = true => can build s[:i] using words from dict (excluding char s[i])
+        # return dp[-1]
+        n = len(s)
+        dp = [False] * (n+1)
+        dp[0] = True
+        for i in range(n+1):
+            if not dp[i]:
+                continue
+            for w in wordDict:
+                lenW = len(w)
+                if i+lenW < n+1 and s[i:i+lenW] == w:
+                    dp[i+lenW] = True
+        return dp[-1]
